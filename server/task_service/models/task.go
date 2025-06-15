8a0cbbb94affb694722f8a5b7e8637415bff68a3
3086953492 <3086953492@qq.com>
2025-06-15 22:31:43 +0800

    refactor(models): 移除TaskHistoryResponse中未使用的字段
    移除了TaskHistoryResponse结构体中的field_name、old_value和new_value字段，这些字段在当前业务逻辑中未被使用，以简化数据结构并减少不必要的序列化开销

--- server/task_service/models/task.go
+++ server/task_service/models/task.go
@@ -118,9 +118,6 @@ type TaskDetailResponse struct {
 type TaskHistoryResponse struct {
 	ID         uint      `json:"id"`
 	Action     string    `json:"action"`
-	FieldName  string    `json:"field_name"`
-	OldValue   string    `json:"old_value"`
-	NewValue   string    `json:"new_value"`
 	OperatorID uint      `json:"operator_id"`
 	Remark     string    `json:"remark"`
 	CreatedAt  time.Time `json:"created_at"`
